2315836015cdd1d4a0ae717aa2330489b1fcaa94
shubham <shubham@shubhams-MacBook-Air.local>
2023-08-25 23:14:53 +0530

    l

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const fs = require("fs");
-const express = require("express");
+import fs from "fs";
+import express from "express"; 
+import cors from "cors";
+import env from "dotenv"; 
+import {Configuration,OpenAIApi} from "openai"
+env.config();
 const app = express();
-const cors = require("cors");
+app.use(express.json());
 
 app.use(cors());
 
@@ -9,7 +13,6 @@ app.get('/', (req, res) => {
     const filePath = "questions.json";
     fs.promises.readFile(filePath, 'utf-8').then((data) => {
         const parsedData = JSON.parse(data);
-        // console.log(parsedData);
         res.json(parsedData); // Use res.json() here
     }).catch((error) => {
         console.error("Error reading JSON file:", error);
@@ -17,6 +20,34 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(3000|| process.env.PORT, () => {
-    console.log("Server is running on port 3000");
+app.get('/hack/:ques', async (req, res) => {
+    try {
+        const ques = req.params.ques;
+        const config = new Configuration({
+            apiKey: process.env.open_api
+        });
+        
+        const openai = new OpenAIApi(config);
+        
+        const conversation = [
+            { role: "user", content: ques },
+            { role: "assistant", content: "This is the system message." }
+        ];
+        
+        const chatCompletion = await openai.createChatCompletion({
+            model: "gpt-4",
+            messages: conversation
+        });
+        
+        const answer = chatCompletion.data.choices[0].message.content;
+        
+        res.json({ answer });
+    } catch (error) {
+        console.error("Error in /hack route:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+const port = process.env.PORT || 3000; // Set the port correctly
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
